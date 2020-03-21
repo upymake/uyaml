9559e728708b99b50a6303bdee15066c541a6c26
Volodymyr Yahello <vyahello@gmail.com>
2020-03-21 20:30:42 +0200

    Include friendly context connection

---
+++ uyaml/connection.py
@@ -0,0 +1,23 @@
+"""Module contains interfaces to work with connections."""
+from abc import abstractmethod
+from types import TracebackType
+from typing import ContextManager, Any, Optional, Type
+
+
+class Friendly(ContextManager["Friendly"]):
+    """An interface describing a friendly context manager connection."""
+
+    @abstractmethod
+    def __enter__(self) -> Any:
+        """Returns connection itself."""
+        pass
+
+    @abstractmethod
+    def __exit__(
+        self,
+        exception_type: Optional[Type[BaseException]],
+        exception_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        """Closes connection itself."""
+        pass
