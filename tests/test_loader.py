b8e310ddf20684339e486879b774450f9161dad3
Volodymyr Yahello <vyahello@gmail.com>
2020-04-23 14:59:59 +0300

    Implement context yaml manager from path

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -4,7 +4,7 @@ import pytest
 from _pytest.tmpdir import TempdirFactory
 from py._path.local import LocalPath
 from uyaml.file import File
-from uyaml.loader import YamlFromPath, Yaml, YamlFromStream
+from uyaml.loader import YamlFromPath, Yaml, YamlFromStream, ContextYamlFromPath
 
 
 @pytest.fixture(scope="session")
@@ -31,6 +31,12 @@ def yaml_from_path(path: LocalPath) -> YamlFromPath:
 
 
 @pytest.fixture(scope="session")
+def context_yaml_from_path(path: LocalPath) -> YamlFromPath:
+    with ContextYamlFromPath(str(path)) as yaml:  # type: IO[str]
+        yield yaml
+
+
+@pytest.fixture(scope="session")
 def yaml_from_stream(path: LocalPath) -> YamlFromStream:
     with File(str(path)) as file:  # type: IO[str]
         yield YamlFromStream(file)
@@ -40,24 +46,41 @@ def test_parsed(yaml_from_path: YamlFromPath) -> None:
     assert isinstance(yaml_from_path._parsed, Yaml)
 
 
-def test_from_path_content(yaml_from_path: YamlFromPath) -> None:
+def test_from_path_content(yaml_from_path: Yaml) -> None:
     assert yaml_from_path.content() == {
         "top": {"foo": {"content": "empty", "priority": 0}, "bar": {"content": "empty", "priority": 1}}
     }
 
 
-def test_from_path_section(yaml_from_path: YamlFromPath) -> None:
+def test_from_path_section(yaml_from_path: Yaml) -> None:
     assert yaml_from_path.section("top") == {
         "foo": {"content": "empty", "priority": 0},
         "bar": {"content": "empty", "priority": 1},
     }
 
 
-def test_from_stream_content(yaml_from_stream: YamlFromStream) -> None:
+def test_from_stream_content(yaml_from_stream: Yaml) -> None:
     assert yaml_from_stream.content() == {
         "top": {"foo": {"content": "empty", "priority": 0}, "bar": {"content": "empty", "priority": 1}}
     }
 
 
-def test_from_stream_section(yaml_from_stream: YamlFromStream) -> None:
+def test_from_stream_section(yaml_from_stream: Yaml) -> None:
     assert yaml_from_stream.section("top")["foo"] == {"content": "empty", "priority": 0}
+
+
+def test_context_from_path_content(context_yaml_from_path: Yaml) -> None:
+    assert context_yaml_from_path.content() == {
+        "top": {"foo": {"content": "empty", "priority": 0}, "bar": {"content": "empty", "priority": 1}}
+    }
+
+
+def test_context_from_path_section(context_yaml_from_path: Yaml) -> None:
+    assert context_yaml_from_path.section("top") == {
+        "foo": {"content": "empty", "priority": 0},
+        "bar": {"content": "empty", "priority": 1},
+    }
+
+
+def test_context_from_path_head(context_yaml_from_path: ContextYamlFromPath) -> None:
+    assert isinstance(context_yaml_from_path._head, Yaml)
--- uyaml/loader.py
+++ uyaml/loader.py
@@ -1,8 +1,10 @@
 """Module represents API to load data from specific YAML steam."""
 from abc import ABC, abstractmethod
-from typing import Any, Dict, IO
+from types import TracebackType
+from typing import Any, IO, Dict, Optional, Type
+from punish.type import AbstractContextManager
 from yaml import safe_load
-from uyaml.file import File, safe_path
+from uyaml.file import YamlFile
 from uyaml.type import List
 
 YamlType = Dict[Any, Any]
@@ -43,7 +45,7 @@ class YamlFromPath(Yaml):
     """Represents a filepath as a `YAML` object."""
 
     def __init__(self, path: str) -> None:
-        self._path: str = safe_path(path, extensions=("yml", "yaml"))
+        self._path: str = path
         self._content: List = List()
 
     def content(self) -> YamlType:
@@ -56,6 +58,41 @@ class YamlFromPath(Yaml):
     def _parsed(self) -> Yaml:
         """Returns parsed YAML content."""
         if not self._content:
-            with File(self._path) as file:  # type: IO[str]
+            with YamlFile(self._path) as file:  # type: IO[str]
                 self._content.append(YamlFromStream(file))
         return self._content.first()
+
+
+class ContextYamlFromPath(AbstractContextManager, Yaml):
+    """Represents a filepath as a `YAML` context manager object."""
+
+    def __init__(self, path: str) -> None:
+        self._path: str = path
+        self._content: List = List()
+
+    def __enter__(self) -> Yaml:
+        """Returns YAML content itself."""
+        if not self._content:
+            with YamlFile(self._path) as file:  # type: IO[str]
+                self._content.append(YamlFromStream(file))
+        return self
+
+    def content(self) -> YamlType:
+        return self._head.content()
+
+    def section(self, name: str) -> YamlType:
+        return self._head.section(name)
+
+    @property
+    def _head(self) -> Yaml:
+        """Returns head of yaml content."""
+        return self._content.first()
+
+    def __exit__(
+        self,
+        exception_type: Optional[Type[BaseException]],
+        exception_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        """Clears YAML content."""
+        self._content.clear()
